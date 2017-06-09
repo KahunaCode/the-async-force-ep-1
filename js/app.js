b3d84a7bac87bc355edfa37714c56c2ff2a5037c
James Moore <jameslmoore@gmail.com>
2017-06-08 18:09:35 -1000

    person 14 name/species

--- js/app.js
+++ js/app.js
@@ -35,4 +35,40 @@
     getPerson(4);
   })();
 
+  //iife for person14 innerhtmls
+  (function (){
+    function reqListener(){
+      var response = JSON.parse(this.responseText);
+      (function(){
+        function reqListener2(){
+          var response = JSON.parse(this.responseText);
+          var p4hw = document.getElementById("person14Species");
+          p4hw.innerHTML = response.name;
+        }
+
+        function getSpecies(id){
+          var oReq = new XMLHttpRequest();
+          oReq.addEventListener('load', reqListener2);
+          oReq.open('GET', id);
+          oReq.send();
+        }
+        getSpecies(response.species);
+
+      })();
+      var p14n = document.getElementById("person14Name");
+      p14n.innerHTML = response.name;
+    }
+
+    function getPerson(id){
+      var oReq = new XMLHttpRequest();
+      oReq.addEventListener('load', reqListener);
+      oReq.open('GET', `http://swapi.co/api/people/${id}`);
+      oReq.send();
+    }
+    getPerson(14);
+  })();
+
+
+
+
 })();
