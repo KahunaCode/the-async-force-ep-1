7f872bbd1d5446393d647ffff073294f9950e649
James Moore <jameslmoore@gmail.com>
2017-06-08 18:00:05 -1000

    person4 and person4homeworld done

--- js/app.js
+++ js/app.js
@@ -5,6 +5,25 @@
   function reqListener(){
     var response = JSON.parse(this.responseText);
     console.log(response.homeworld);
+
+    (function(){
+      //console.log("iife "+response.homeworld);
+      function reqListener2(){
+        var response = JSON.parse(this.responseText);
+        var p4hw = document.getElementById("person4HomeWorld");
+        p4hw.innerHTML = response.name;
+      }
+
+      function getHomeworld(id){
+        var oReq = new XMLHttpRequest();
+        oReq.addEventListener('load', reqListener2);
+        //oReq.open('GET', `http://swapi.co/api/people/${id}`);
+        oReq.open('GET', id);
+        oReq.send();
+      }
+      getHomeworld(response.homeworld);
+
+    })();
     var p4n = document.getElementById("person4Name");
     p4n.innerHTML = response.name;
   }
@@ -16,22 +35,22 @@
     oReq.send();
   }
 
-  function reqListener2(){
-    var response = JSON.parse(this.responseText);
-    var p4hw = document.getElementById("person4HomeWorld");
-    p4hw.innerHTML = response.name;
-  }
+  // function reqListener2(){
+  //   var response = JSON.parse(this.responseText);
+  //   var p4hw = document.getElementById("person4HomeWorld");
+  //   p4hw.innerHTML = response.name;
+  // }
 
-  function getHomeworld(id){
-    var oReq = new XMLHttpRequest();
-    oReq.addEventListener('load', reqListener2);
-    oReq.open('GET', `http://swapi.co/api/people/${id}`);
-    oReq.send();
-  }
+  // function getHomeworld(id){
+  //   var oReq = new XMLHttpRequest();
+  //   oReq.addEventListener('load', reqListener2);
+  //   oReq.open('GET', `http://swapi.co/api/people/${id}`);
+  //   oReq.send();
+  // }
 
 
 getPerson(4);
-getHomeworld(1);
+// getHomeworld(1);
 
 
 })();
